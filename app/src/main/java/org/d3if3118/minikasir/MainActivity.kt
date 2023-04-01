54cfd43c8511b458a0a25880eda0fc74971d6c38
“Firmansyah <firmansyahseptian@student.telkomuniversity.ac.id>
2023-04-01 14:10:44 +0700

    meminta inputan dari pengguna

--- app/src/main/java/org/d3if3118/minikasir/MainActivity.kt
+++ app/src/main/java/org/d3if3118/minikasir/MainActivity.kt
@@ -2,10 +2,28 @@ package org.d3if3118.minikasir
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.d3if3118.minikasir.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Initialize the binding object
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Add click listener to the "Proses" button
+        binding.btnProses.setOnClickListener {
+            // Get input values from the EditText views
+            val namaPelanggan = binding.etNamaPelanggan.text.toString()
+            val namaBarang = binding.etNamaBarang.text.toString()
+            val jumlahBeli = binding.etJumlahBeli.text.toString().toInt()
+            val harga = binding.etHarga.text.toString().toInt()
+            val uangBayar = binding.etUangBayar.text.toString().toInt()
+
+        }
     }
 }
